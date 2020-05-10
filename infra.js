f51ebd2e09c79bfb356f97cfc6c14c03a077a82d
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-10 05:43:41 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,27 +1,25 @@
-import Popup from '/vendor/infrajs/popup/Popup.js'
-import Wait from '/vendor/akiyatkin/load/Wait.js'
+import { Popup } from '/vendor/infrajs/popup/Popup.js'
+import { Event } from '/vendor/infrajs/event/Event.js'
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
 
-Wait().then(() => {
+Event.handler('Layer.isshow', function (layer) {
+	do {
+		if (layer.popupis === false) return false;
+		layer = layer.parent;
+	} while (layer)
+}, 'popup');
 
-	Event.handler('Layer.isshow', function (layer) {
-		do {
-			if (layer.popupis === false) return false;
-			layer = layer.parent;
-		} while (layer)
-	}, 'popup');
+Event.handler('Controller.onshow', async () => {
+	Popup.render()
+}, 'popup');
 
-	Event.handler('Controller.onshow', async () => {
-		Popup.render()
-	}, 'popup');
-
-	Controller.popup_memorize = function (code) {
-		if (!popup.st) return;
-		Controller.code_save('popup', code);
-		popup.div.on('hidden.bs.modal', function () {
-			Controller.code_remove('popup', code);
-		});
-		//Event.onext('Layer.onhide',function(){
-		//Controller.code_remove('popup',code);
-		//},'',popup.st.obj);
-	}
-})
+Controller.popup_memorize = function (code) {
+	if (!popup.st) return;
+	Controller.code_save('popup', code);
+	popup.div.on('hidden.bs.modal', function () {
+		Controller.code_remove('popup', code);
+	});
+	//Event.onext('Layer.onhide',function(){
+	//Controller.code_remove('popup',code);
+	//},'',popup.st.obj);
+}
